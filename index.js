e794546a4c2f7d7264ef5db699bf9366ae73c4d5
Adam Robson <adamray312@gmail.com>
2023-01-12 10:30:33 -0800

    function

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+let i = 0;
+while (i <= 100) {
+  i++;
+  console.log(i);
+  if (i % 3 === 0) {
+    console.log('fizz');
+  } else if (i % 5 === 0) {
+    console.log('buzz');
+  } else if (i % 3 === 0 || i % 5 === 0) {
+    console.log('fizzbuzz');
+  } else {
+    console.log(i)
+  }
+}
